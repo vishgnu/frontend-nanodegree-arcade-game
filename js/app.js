ac211d11f3c4674bc776d0fe7c1ff9f5e503c811
vishgnu <niels.walter@gmail.com>
2015-08-13 23:20:41 +0200

    player steps and borders working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,13 +25,84 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x,y) {
+    this.x = x || 200;
+    this.y = y || 400;
+    this.sprite = 'images/char-boy.png';
+    this.input = null;
+}
+
+Player.prototype.restart = function () {
+    this.x = 200;
+    this.y = 400;
+}
+
+// Update for player
+Player.prototype.update = function (dt) {
+
+    var yStep = 82;
+    var xStep = 100;
+
+    if(this.input != null)
+    {
+        switch (this.input) {
+            case "up":
+                // check completed
+                if (this.y < 0) {
+                    this.restart();
+                }
+                else {
+                    this.y -= yStep;
+                }
+                break;
+            case "down":
+                if (this.y < 400) {
+                    this.y += yStep;
+                 }
+                break;
+            case "left":
+                if (this.x > 0) {
+                    this.x -= xStep;
+                }
+                break;
+            case "right":
+                if (this.x < 400) {
+                    this.x += xStep;
+                }
+                break;
+            default:
+                break;
+        }
+
+        this.input = null;
+    }
+}
+
+// render player
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    console.log("Player" + this.x, this.y);
+}
+
+
+Player.prototype.handleInput = function (keycode) {
+    // call position update for player here
+    this.input = keycode;
+}
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [
+    new Enemy(0, 60),
+    new Enemy(0, 150),
+    new Enemy(0, 230),
+    new Enemy(0, 315)];
+
 // Place the player object in a variable called player
 
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
